d42c4bbd434a0e4318eb18ad9334c1d7250046c4
viinir <3hawks@rambler.ru>
2018-10-08 12:04:59 +0300

    Third test commit, adding saiku and pgadmin launch commands, fixing function for comparing two text files (.xml, .txt, etc.), adding temp file output for compare results instead of console output.

--- Gui1.java
+++ Gui1.java
@@ -3,9 +3,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.util.Scanner;
 
 public class Gui1 extends JFrame {
     private JPanel panel;
+    private JButton button1;
     private JTextField textField1;
     private JPasswordField passwordField1;
 
@@ -30,6 +32,8 @@ public class Gui1 extends JFrame {
         JPanel panel = new JPanel();
         //panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
+        JTextField textField;
+
         panel.add(Box.createVerticalGlue());
 
         final JLabel label1 = new JLabel("Выбранный файл");
@@ -44,10 +48,12 @@ public class Gui1 extends JFrame {
 
         panel.add(Box.createRigidArea(new Dimension(10, 10)));
 
-        JButton path_button1 = new JButton("Показать JFileChooser");
+        textField = new JTextField(8);
+
+        JButton path_button1 = new JButton("JFileChooser");
         path_button1.setAlignmentX(LEFT_ALIGNMENT);
 
-        JButton path_button2 = new JButton("Показать JFileChooser");
+        JButton path_button2 = new JButton("JFileChooser");
         path_button2.setAlignmentX(RIGHT_ALIGNMENT);
 
         JButton compare_button = new JButton("Сравнить");
@@ -55,6 +61,12 @@ public class Gui1 extends JFrame {
 
         JButton mdx_button = new JButton("Запуск MDX");
         mdx_button.setAlignmentX(RIGHT_ALIGNMENT);
+
+        JButton saiku_button = new JButton("Запуск Saiku");
+        saiku_button.setAlignmentX(RIGHT_ALIGNMENT);
+
+        JButton pgadmin_button = new JButton("Запуск СУБД");
+        pgadmin_button.setAlignmentX(RIGHT_ALIGNMENT);
 
         path_button1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -85,44 +97,52 @@ public class Gui1 extends JFrame {
 
                 try {
 
+                    //PrintStream fileStream = new PrintStream("C:/Users/Lankayylas/Desktop/Univer/pgu/other/output.txt");
+
                     BufferedReader reader1 = new BufferedReader(new FileReader(label1.getText()));
                     BufferedReader reader2 = new BufferedReader(new FileReader(label2.getText()));
                     String line1 = reader1.readLine();
                     String line2 = reader2.readLine();
 
                     boolean areEqual = true;
-                    int lineNum = 1;
+                    int lineNum = 1, lineTotal = 1;
 
-                    while (line1 != null || line2 != null)
+                    PrintStream ps = new PrintStream("C:/Users/Lankayylas/Desktop/Univer/pgu/other/output.txt");
+                    PrintStream orig = System.out;
+
+                    do
                     {
-                        if(line1 == null || line2 == null)
+                        //System.out.println("line1: "+line1); System.out.println("line2: "+line2);
+                        System.setOut(ps);
+                        if(line2 == null)
                         {
                             areEqual = false;
+                            System.out.println("Reached end of file2 before file1 ended as well.");
                             break;
+
                         }
-                        else if(! line1.equalsIgnoreCase(line2))
+                        else if(! line1.equals(line2))
                         {
                             areEqual = false;
-                            break;
-                        }
+                            System.out.println("Two files have different content. They differ at line "+lineNum);
+                            System.out.println(label1.getText()+" has "+line1+" and "+label2.getText()+" has "+line2+" at line "+lineNum);
+
+                        } else if (line1.equals(line2)) {lineTotal++; /*System.out.println("lineTotal: "+lineTotal);*/}
 
                         line1 = reader1.readLine();
                         line2 = reader2.readLine();
                         lineNum++;
-                    }
-
-                    if(areEqual)
+                        //System.out.println("lineNum: "+lineNum);
+                    } while (line1 != null);
+                    if (lineNum == lineTotal)
                     {
                         System.out.println("Two files have same content.");
-                    }
-                    else
-                    {
-                        System.out.println("Two files have different content. They differ at line "+lineNum);
-                        System.out.println("File1 has "+line1+" and File2 has "+line2+" at line "+lineNum);
                     }
 
                     reader1.close();
                     reader2.close();
+                    System.setOut(orig);
+                    ps.close();
 
                 } catch (IOException ee) {
                     ee.printStackTrace();
@@ -143,24 +163,41 @@ public class Gui1 extends JFrame {
             }
         });
 
-        /*pgadmin_button.addActionListener(new ActionListener() {
+        saiku_button.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent actionEvent) {
 
                 try {
                     Process p = Runtime.getRuntime().exec(
-                            "cmd /c start C:/Users/Lankayylas/Desktop/Univer/pgu/other/schema-workbench/workbench.bat");
+                            "cmd /c start C:/Users/Lankayylas/Desktop/Univer/pgu/saiku-server/start-saiku.bat");
                 } catch (IOException e){
                     e.printStackTrace();
                 }
             }
-        });*/
+        });
 
+        //C:\Users\Lankayylas\Desktop\Univer\pgu\saiku-server\start-saiku.bat
+
+        pgadmin_button.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                try {
+                    Process process = new ProcessBuilder("C:\\Program Files\\PostgreSQL\\10\\pgAdmin 4\\bin\\pgAdmin4.exe").start();
+                    //Process p = Runtime.getRuntime().exec(new String[] {"cmd", "/c", "start", "C:\\Program Files\\PostgreSQL\\10\\pgAdmin 4\\bin\\pgAdmin4.exe"});
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        panel.add(textField);
         panel.add(path_button1);
         panel.add(path_button2);
         panel.add(compare_button);
         panel.add(mdx_button);
-        //panel.add(pgadmin_button);
+        panel.add(saiku_button);
+        panel.add(pgadmin_button);
         panel.add(Box.createVerticalGlue());
         getContentPane().add(panel);
 
